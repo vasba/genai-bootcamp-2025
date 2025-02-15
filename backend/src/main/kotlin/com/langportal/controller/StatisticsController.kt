cef25f634f71fecfa5095863a0ac7948b1c9d04d
Vasile Baluta <vasile.baluta@volvocars.com>
2025-02-15 23:28:40 +0100

    gh-5 c12
    add statistics controller tests

--- backend/src/main/kotlin/com/langportal/controller/StatisticsController.kt
+++ backend/src/main/kotlin/com/langportal/controller/StatisticsController.kt
@@ -3,7 +3,10 @@ package com.langportal.controller
 import com.langportal.dto.ReviewStatsDTO
 import com.langportal.service.StatisticsService
 import org.springframework.http.ResponseEntity
-import org.springframework.web.bind.annotation.*
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
 
 @RestController
 @RequestMapping("/statistics")
