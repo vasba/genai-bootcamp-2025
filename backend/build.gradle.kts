eb19421731efbf14e155e3702e910c0314e9aed2
Vasile Baluta <vasile.baluta@volvocars.com>
2025-02-15 20:28:43 +0100

    gh-5 c3
    Refactor main class to comply with java syntax as requirted by
    Spring Boot

--- backend/build.gradle.kts
+++ backend/build.gradle.kts
@@ -40,3 +40,7 @@ tasks.withType<KotlinCompile> {
 tasks.withType<Test> {
     useJUnitPlatform()
 }
+
+springBoot {
+    mainClass.set("com.langportal.LangPortalApplicationKt")
+}
--- backend/src/main/kotlin/com/langportal/LangPortalApplication.kt
+++ backend/src/main/kotlin/com/langportal/LangPortalApplication.kt
@@ -4,8 +4,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
 @SpringBootApplication
-class LangPortalApplication
-
-fun main(args: Array<String>) {
-    runApplication<LangPortalApplication>(*args)
+class LangPortalApplication {
+    companion object {
+        @JvmStatic
+        fun main(args: Array<String>) {
+            runApplication<LangPortalApplication>(*args)
+        }
+    }
 }
