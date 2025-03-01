0af14e55c08537b1a31024c8174d95fd4a3b126f
Vasile Baluta <vasile.baluta@volvocars.com>
2025-03-02 00:32:33 +0100

    gh-8 c15 fix study activity backend
    Many to many doublelinked relationship causes stack overflow

--- backend/src/main/kotlin/com/langportal/model/StudyActivity.kt
+++ backend/src/main/kotlin/com/langportal/model/StudyActivity.kt
@@ -8,13 +8,8 @@ data class StudyActivity(
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     val id: Long? = null,
-
     @Column(nullable = false)
     val name: String,
-
     @Column(nullable = false)
     val url: String,
-
-    @OneToMany(mappedBy = "studyActivity", cascade = [CascadeType.ALL], fetch = FetchType.LAZY)
-    val sessions: MutableList<StudySession> = mutableListOf()
 )
